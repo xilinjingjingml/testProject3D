2e3e2e9663fad580303eac4e900427b28cf98135
夏菁 <xiajing@microbeam.cn>
2021-08-09 17:20:17 +0800

    首次提交

---
+++ assets/start/GameManager.ts
@@ -0,0 +1,71 @@
+import { _decorator, Component, Prefab, instantiate, Node, CCInteger} from "cc";
+const { ccclass, property } = _decorator;
+
+enum BlockType{
+    BT_NONE,
+    BT_STONE,
+};
+
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+
+    @property({type: Prefab})
+    public cubePrfb: Prefab | null = null;
+    @property({type: CCInteger})
+    public roadLength: Number = 50;
+    @property({type: PlayerController})
+    public playerCtrl: PlayerController = null;
+    private _road: number[] = [];
+
+    private _curState: GameState = GameState.GS_INIT;
+
+    start () {
+        this.generateRoad();
+    }
+
+    generateRoad() {
+
+        this.node.removeAllChildren();
+
+        this._road = [];
+        // startPos
+        this._road.push(BlockType.BT_STONE);
+
+        for (let i = 1; i < this.roadLength; i++) {
+            if (this._road[i-1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+
+        for (let j = 0; j < this._road.length; j++) {
+            let block: Node = this.spawnBlockByType(this._road[j]);
+            if (block) {
+                this.node.addChild(block);
+                block.setPosition(j, -1.5, 0);
+            }
+        }
+    }
+
+    spawnBlockByType(type: BlockType) {
+        let block = null;
+        switch(type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.cubePrfb);
+                break;
+        }
+
+        return block;
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+}
---
+++ assets/start/start/PlayerController.ts
@@ -0,0 +1,82 @@
+
+import { _decorator, Component, Vec3, systemEvent, SystemEvent, EventMouse, Animation, v3, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _curJumpTime: number = 0;
+    private _jumpTime: number = 0.1;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = v3();
+    private _deltaPos: Vec3 = v3(0, 0, 0);
+    private _targetPos: Vec3 = v3();
+    private _isMoving = false;
+
+    //body动画
+    @property({type: Animation})
+   public BodyAnim: Animation | null = null;
+
+    start () {
+        // systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+
+    }
+
+    jumpByStep(step: number) {
+        if (this._isMoving) {
+            return;
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, v3(this._jumpStep, 0, 0));
+
+        if (step === 1) {
+            this.BodyAnim && this.BodyAnim.play('oneStep');
+         } else if (step === 2) {
+            this.BodyAnim &&  this.BodyAnim.play('twoStep');
+         }
+        this._isMoving = true;
+    }
+
+    onOnceJumpEnd() {
+        this._isMoving = false;
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }    
+
+    setInputActive(active: boolean) {
+        if (active) {
+            systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            systemEvent.off(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
+    }
+}
